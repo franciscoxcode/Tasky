b556693781a52e538dc8f3cf3ae1914111cd1d9a
Francisco Casillas <chiccasillas@gmail.com>
2025-09-23 13:09:24 -0600

    feat(TabView): add three tabs for Events, Tasks, and Notes views

---
+++ Tasky/Views/NotesView.swift
@@ -0,0 +1,15 @@
+//
+//  NotesView.swift
+//  Tasky
+//
+//  Created by Francisco Jean on 23/09/25.
+//
+
+import SwiftUI
+
+struct NotesView: View {
+    var body: some View {
+        Text("Notes Page")
+            .font(.largeTitle)
+    }
+}
