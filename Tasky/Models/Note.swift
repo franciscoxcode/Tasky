677516a1b3e5314bcecccc3a09bad92ea49c514f
Francisco Casillas <chiccasillas@gmail.com>
2025-09-30 17:21:56 -0600

    feat(Note): add Note model with title, content, and project relation

---
+++ Tasky/Models/Note.swift
@@ -0,0 +1,16 @@
+//
+//  Note.swift
+//  Tasky
+//
+//  Created by Francisco Jean on 30/09/25.
+//
+import Foundation
+
+struct Note: Identifiable {
+    let id = UUID()
+    var title: String? = nil
+    var content: String
+    let createdAt = Date()
+    var updatedAt = Date()
+    var projectID: UUID
+}
